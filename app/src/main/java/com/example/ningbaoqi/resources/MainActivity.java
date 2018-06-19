74b7d5bece53728873be780fa7577abb3a31736f
ningbaoqi <baoqi.ning@new-bund.net>
2018-06-19 17:55:07 +0800

    数组资源

--- app/src/main/java/com/example/ningbaoqi/resources/MainActivity.java
+++ app/src/main/java/com/example/ningbaoqi/resources/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ningbaoqi.resources;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //获取字符串数组资源
+        String[] stringArray = getResources().getStringArray(R.array.strings_array);
+
+        //获取整数数组资源
+        int[] intArray = getResources().getIntArray(R.array.integer_arrays);
+
+        //获取普通数组资源
+        TypedArray typedArray = getResources().obtainTypedArray(R.array.mipmap_array);
+        ImageView iv1 = new ImageView(this);
+        iv1.setImageDrawable(typedArray.getDrawable(0));
+        typedArray.recycle();
     }
 }
