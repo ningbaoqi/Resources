6cb8fbb0b4110b36a83b840d60636967d886bb0e
ningbaoqi <1031297081@qq.com>
2018-07-26 20:18:19 +0800

    ScaleDrawable

--- app/src/main/java/com/example/ningbaoqi/resources/Activity1.java
+++ app/src/main/java/com/example/ningbaoqi/resources/Activity1.java
@@ -1,9 +1,11 @@
 package com.example.ningbaoqi.resources;
 
+import android.graphics.drawable.ScaleDrawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class Activity1 extends AppCompatActivity {
@@ -15,5 +17,9 @@ public class Activity1 extends AppCompatActivity {
         TransitionDrawable drawable = (TransitionDrawable) textView.getBackground();
         drawable.startTransition(1000);//实现淡入淡出效果
         drawable.reverseTransition(1000);//逆过程
+
+        View view = findViewById(R.id.txt_scale);
+        ScaleDrawable drawable1 = (ScaleDrawable) view.getBackground();
+        drawable.setLevel(1);//必须设置等级
     }
 }
