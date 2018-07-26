8679147050a2c4effb44da51a3a26e9f12001998
ningbaoqi <1031297081@qq.com>
2018-07-26 21:29:40 +0800

    CustomDrawable

---
+++ app/src/main/java/com/example/ningbaoqi/resources/CustomDrawable.java
@@ -0,0 +1,42 @@
+package com.example.ningbaoqi.resources;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+
+public class CustomDrawable extends Drawable {
+    private Paint mPaint;
+
+    public CustomDrawable(int color) {
+        this.mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(color);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        final Rect r = getBounds();
+        float cx = r.exactCenterX();
+        float cy = r.exactCenterY();
+        canvas.drawCircle(cx, cy, Math.min(cx, cy), mPaint);
+    }
+
+    @Override
+    public void setAlpha(int i) {
+        mPaint.setAlpha(i);
+        invalidateSelf();
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        mPaint.setColorFilter(colorFilter);
+        invalidateSelf();
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
